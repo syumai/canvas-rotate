d7d616b5061abde2dd43e202e7afe8eddee89de9
syumai <syumai@gmail.com>
2018-09-27 02:04:04 +0900

    Renamed repo and added new example

--- README.md
+++ README.md
@@ -1,9 +1,50 @@
-# canvas-rotate
+# exif-orientation-fixer
 
-* rotate image using JavaScript.
+## What is this?
 
-## Image source
+- A JavaScript code to fix image orientation based on Exif data.
 
-* https://github.com/recurser/exif-orientation-examples
-  - Author: Dave Perrett :: hello@daveperrett.com :: [@daveperrett](http://twitter.com/daveperrett)
+## Dependency
 
+- [Exif.js](https://github.com/exif-js/exif-js)
+
+## Example
+
+### Basic Example
+
+- https://syumai.github.io/exif-orientation-fixer/examples/basic.html
+
+#### Image source of basic example
+
+- [exif-orientation-examples](https://github.com/recurser/exif-orientation-examples)
+
+### File picker
+
+- https://syumai.github.io/exif-orientation-fixer/examples/file-picker.html
+
+## Usage
+
+```html
+<script src="https://unpkg.com/exif-js@2.3.0/exif.js"></script>
+<script type="module">
+  import { fixImageOrientation } from 'exif-orientation-fixer.js';
+
+  const imagePath = 'https://raw.githubusercontent.com/recurser/exif-orientation-examples/master/Landscape_1.jpg';
+
+  (async () => {
+    const blob = await fixImageOrientation(imagePath);
+    const url = URL.createObjectURL(blob);
+    const img = document.createElement("img");
+    img.src = url;
+    document.body.appendChild(img);
+  })();
+</script>
+```
+
+## LICENSE
+
+MIT
+
+## Author
+
+syumai
--- exif-orientation-fixer.js
+++ exif-orientation-fixer.js
@@ -1,22 +1,28 @@
-const container = document.getElementById("container");
+export function fixImageOrientation(src) {
+  if (src instanceof Blob) {
+    src = URL.createObjectURL(src);
+  }
 
-function draw(canvas, src) {
+  const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
   const img = new Image();
-  return new Promise(r => {
-    img.onload = function () {
-      EXIF.getData(img, function () {
-        const { Orientation } = EXIF.getAllTags(this);
+  img.crossOrigin = 'anonymous';
+
+  return new Promise(resolve => {
+    img.onload = () => {
+      EXIF.getData(img, () => {
+        const orientation = EXIF.getTag(img, 'Orientation') || 1;
         const aspectRatio = img.height / img.width;
-        console.log(Orientation);
-        if (Orientation >= 1 && Orientation <= 4) {
+
+        if (orientation >= 1 && orientation <= 4) {
           canvas.width = img.width;
           canvas.height = img.height;
         } else {
           canvas.width = img.height;
           canvas.height = img.width;
         }
-        switch (Orientation) {
+
+        switch (orientation) {
           case 1:
             break;
           case 2:
@@ -24,7 +30,7 @@ function draw(canvas, src) {
             ctx.scale(-1, 1);
             break;
           case 3:
-            ctx.rotate(180 * Math.PI / 180);
+            ctx.rotate((180 * Math.PI) / 180);
             ctx.translate(-1 * img.width, -1 * img.height);
             break;
           case 4:
@@ -34,46 +40,32 @@ function draw(canvas, src) {
           case 5:
             ctx.translate(img.width * aspectRatio, 0);
             ctx.scale(-1, 1);
-            ctx.rotate(90 * Math.PI / 180);
+            ctx.rotate((90 * Math.PI) / 180);
             ctx.translate(0, -1 * img.height);
             break;
           case 6:
-            ctx.rotate(90 * Math.PI / 180);
+            ctx.rotate((90 * Math.PI) / 180);
             ctx.translate(0, -1 * img.height);
             break;
           case 7:
             ctx.translate(0, img.height / aspectRatio);
             ctx.scale(1, -1);
-            ctx.rotate(90 * Math.PI / 180);
+            ctx.rotate((90 * Math.PI) / 180);
             ctx.translate(0, -1 * img.height);
             break;
           case 8:
             ctx.translate(img.width * aspectRatio, img.height / aspectRatio);
             ctx.scale(-1, -1);
-            ctx.rotate(90 * Math.PI / 180);
+            ctx.rotate((90 * Math.PI) / 180);
             ctx.translate(0, -1 * img.height);
             break;
         }
-        ctx.drawImage(img,0,0);
+
+        ctx.drawImage(img, 0, 0);
+        canvas.toBlob(resolve);
       });
-      r(img);
     };
+
     img.src = src;
   });
 }
-
-(async () => {
-  const imagePaths = [];
-  for (let i = 1; i <= 8; i++) {
-    imagePaths.push(`https://raw.githubusercontent.com/recurser/exif-orientation-examples/master/Landscape_${i}.jpg`);
-    imagePaths.push(`https://raw.githubusercontent.com/recurser/exif-orientation-examples/master/Portrait_${i}.jpg`);
-  }
-
-  for (const imagePath of imagePaths) {
-    const div = document.createElement("div");
-    const canvas = document.createElement("canvas");
-    const img = await draw(canvas, imagePath);
-    div.appendChild(canvas);
-    container.appendChild(div);
-  }
-})();
